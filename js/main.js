40196a493be92ca552222d6094df5dae7f53b31f
Pavel Kuznetsov <pavel.kuznetsov.work@gmail.com>
2021-11-14 15:31:48 +0300

    add ajax form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import './thumbnails.js';
 import './new-photo-modal.js';
 import './fullsize-photo-effects.js';
+import {getData} from './api.js';
+import {showErrorMessage} from './api.js';
+import {getUserPhotoDescriptionsList} from './thumbnails.js';
+
+getData(getUserPhotoDescriptionsList, showErrorMessage);
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,24 +1,26 @@
-import {getUserPhotoDescriptionsList} from './data.js';
+// import {getUserPhotoDescriptionsList} from './data.js';
 import {openFullsizePhotoModal} from './fullsize-photo-modal.js';
 
 const userPhotoThumbnailElement = document.querySelector('.pictures');
 const userPhotoThumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const userPhotoThumbnails = getUserPhotoDescriptionsList();
-const userPhotoThumbnailElementFragment = document.createDocumentFragment();
+// const userPhotoThumbnails = getUserPhotoDescriptionsList();
 
-userPhotoThumbnails.forEach((userPhoto) => {
-  const userPhotoElement = userPhotoThumbnailTemplate.cloneNode(true);
-  userPhotoElement.querySelector('.picture__img').src = `${userPhoto.url}`;
-  userPhotoElement.querySelector('.picture__likes').textContent = `${userPhoto.likes}`;
-  userPhotoElement.querySelector('.picture__comments').textContent = `${userPhoto.comments.length}`;
-  userPhotoThumbnailElementFragment.appendChild(userPhotoElement);
+const getUserPhotoDescriptionsList = (userPhotoThumbnails) => {
+  const userPhotoThumbnailElementFragment = document.createDocumentFragment();
 
-  userPhotoElement.addEventListener('click', () => {
-    openFullsizePhotoModal(userPhoto);
+  userPhotoThumbnails.forEach((userPhoto) => {
+    const userPhotoElement = userPhotoThumbnailTemplate.cloneNode(true);
+    userPhotoElement.querySelector('.picture__img').src = `${userPhoto.url}`;
+    userPhotoElement.querySelector('.picture__likes').textContent = `${userPhoto.likes}`;
+    userPhotoElement.querySelector('.picture__comments').textContent = `${userPhoto.comments.length}`;
+    userPhotoThumbnailElementFragment.appendChild(userPhotoElement);
+
+    userPhotoElement.addEventListener('click', () => {
+      openFullsizePhotoModal(userPhoto);
+    });
+
   });
+  userPhotoThumbnailElement.appendChild(userPhotoThumbnailElementFragment);
+};
 
-});
-
-userPhotoThumbnailElement.appendChild(userPhotoThumbnailElementFragment);
-
-export {userPhotoThumbnailElement};
+export {getUserPhotoDescriptionsList};
